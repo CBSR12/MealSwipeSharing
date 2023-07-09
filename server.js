e379614af897c1a3a4f36ceb09f209ba7940bc3f
Shiva Rudra <srudra1024@gmail.com>
2023-07-08 17:38:38 -0700

    select menus chosen

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('styles'))
+
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('donatep')
+})
+
+
+app.get('/new', (req, res) => {
+    console.log('suc')
+})
+
+app.post('/new', (req, res) => {
+    console.log(JSON.stringify(req.body.day));
+    console.log( req.body.month + req.body.day + req.body.time + req.body.location + req.body.swipes);
+    res.send(req.body.month);
+    
+    
+    
+})
+
+app.listen(3000, () => {
+    console.log("HI")
+})
